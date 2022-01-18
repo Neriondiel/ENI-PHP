a39d702308a2cbe0fc046172b2d51b23c6b48fcf
Neriondiel <cyril.18.09.97@hotmail.fr>
2022-01-18 16:50:14 +0100

    TP - Etape 1 jusqu'à 6

--- TP-Tableau-Mardi/index.php
+++ TP-Tableau-Mardi/index.php
@@ -1,4 +1,27 @@
 <?php 
+//Etape 1
+$etape1 = range(0, 41);
+var_dump($etape1);
 
+//Etape 2
+shuffle($etape1);
+var_dump($etape1);
 
+//Etape 3
+$etape3 = array_rand($etape1, 16);
+var_dump($etape3);
+
+//Etape 4 
+$etape4 = array_merge($etape3,$etape3);
+var_dump($etape4);
+
+//Etape 5
+shuffle($etape4);
+var_dump($etape4);
+
+//Etape 6
+echo "<br> Majong <br>";
+foreach($etape4 as $nom){
+    echo "<img src='./majhong/images/".$nom.".jpg'>";
+}
 ?>
